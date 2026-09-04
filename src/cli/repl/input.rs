78492ae42fe9f58364079fafb1e0224f37362e66
yury-cvianez <yurycvianez@gmail.com>
2026-09-04 10:52:04 +0000

    add a read loop to the session and tested it

--- src/cli/repl/input.rs
+++ src/cli/repl/input.rs
@@ -2,10 +2,36 @@ pub mod termguard;
 
 use std::os::unix::io::AsRawFd;
 
-pub fn session() {
+pub fn session() -> std::io::Result<()> {
     
     let fd = std::io::stdin().as_raw_fd();
     let _guard = termguard::TermiosGuard::new(fd).unwrap();
     _guard.enable_raw_mode().unwrap();
+
+    let mut buffer = [0u8, 1];
+
+    loop {
+        let n = unsafe {
+            libc::read(
+                fd,
+                buffer.as_mut_ptr() as *mut libc::c_void,
+                1
+            )   
+        };
+        
+        if n < 0 {
+            return Err(std::io::Error::last_os_error());
+        }
+
+        if n == 0 {
+            break;
+        }
+
+        let byte = buffer[0];
+
+        println!("read byte: {byte:#04x}");
+    }
+
+    Ok(())
 
 }
